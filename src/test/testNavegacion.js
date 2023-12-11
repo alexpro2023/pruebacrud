5b4f5d89f8ac46008066401313222f0a12833a9a
Yael-Flores <20200702@uthh.edu.mx>
2023-12-10 22:08:31 -0600

    Test Act

---
+++ src/test/testNavegacion.js
@@ -0,0 +1,14 @@
+import { Selector } from 'testcafe';
+
+fixture('Navegacion Home - CRUD')
+.page('https://pruebacrud.vercel.app/home')
+.beforeEach(async t => {
+    await t.maximizeWindow();
+});
+
+
+test('Navegar a CRUD', async t => {
+    const link = Selector('a').withText('Crud_Menu');
+    await t.expect(link.visible).ok('El enlace no está visible').click(link);
+    await t.wait(10000);
+}).skipJsErrors();
