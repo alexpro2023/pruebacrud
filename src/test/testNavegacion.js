7cd6f9a99ec636a54677100df0659ad5a91ba9ef
CesaarBtoos <cemartz120802@gmail.com>
2023-12-11 08:57:26 -0600

    correido

--- src/test/testNavegacion.js
+++ src/test/testNavegacion.js
@@ -8,7 +8,7 @@ fixture('Navegacion Home - CRUD')
 
 
 test('Navegar a CRUD', async t => {
-    const link = Selector('a').withText('pedro');
+    const link = Selector('a').withText('Crud_Menu');
     await t.expect(link.visible).ok('El enlace no está visible').click(link);
     await t.wait(10000);
 }).skipJsErrors();
