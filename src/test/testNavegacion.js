9a521eb02855d6e7853a8c0853d4c4f8a6eb9da3
Yael-Flores <20200702@uthh.edu.mx>
2023-12-12 22:52:23 -0600

    Commit Test

--- src/test/testNavegacion.js
+++ src/test/testNavegacion.js
@@ -11,4 +11,5 @@ test('Navegar a CRUD', async t => {
     const link = Selector('a').withText('Crud_Menu');
     await t.expect(link.visible).ok('El enlace no está visible').click(link);
     await t.wait(10000);
+    //holas test
 }).skipJsErrors();
